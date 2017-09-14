423f0017815d18614c64e396f08eaf3affaf7cd2
Rochel Levi <rochlevi@gmail.com>
2017-09-14 00:48:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,4 @@
 display_board(board)
-p "{board[0]}|{board[1]}|{board[2]}/n-----------/n{board[3]}|{board[4]}|{board[5]}/n-----------/n
-{board[6]}|{board[7]}|{board[8]}"
+puts ("{board[0]}|{board[1]}|{board[2]}/n-----------/n{board[3]}|{board[4]}|{board[5]}/n-----------/n{board[6]}|{board[7]}|{board[8]}")
 
 end
